4015acc2637711729b832c1e24e2c111e1ac610c
Cedric Falcou <cedricfa@monday.com>
2023-02-28 12:31:48 +0000

    Code Clean Up

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ const App = () => {
     try {
       monday.execute("valueCreatedForUser")
 
-      alert("This is a warning message")
       // set context
       const currentContext = await monday.get("context")
       const currentBlock =
