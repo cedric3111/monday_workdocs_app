09151ddf9a3f22acc344f7a53590646d64f71673
Cedric Falcou <cedricfa@monday.com>
2023-02-28 10:56:31 +0000

    Code cleanup

--- src/App.js
+++ src/App.js
@@ -11,8 +11,6 @@ const monday = mondaySdk()
 
 //This is a test as amending the pull request.
 // TODO Adding more stuff
-// Add Integration to ChatGPT here
-//Changesvcxvxcvxcvddsfsdfdsf fdsfds
 
 const App = () => {
   async function capitalizeText() {
