4790c4dd98cf5a57b167de050d0e24198ac3d401
Cedric Falcou <cedricfa@monday.com>
2023-07-14 22:32:54 +0100

    Test PR Request from Git UI v5

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ const monday = mondaySdk()
 //Test PR Request from Git UI v2
 //Test PR Request from Git UI v3
 //Test PR Request from Git UI v4
+//Test PR Request from Git UI v5
 
 const App = () => {
   async function capitalizeText() {
