2321255f5c0a78d44ed097551994dc3ed3413e77
Cedric Falcou <cedricfa@monday.com>
2023-07-16 11:54:10 +0100

    Add Capitalise All Sentence

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { Button } from "monday-ui-react-core"
 const monday = mondaySdk()
 
 //Add Capitalise Selected Text
+//Add Capitalise All Sentence
 
 const App = () => {
   async function capitalizeText() {
